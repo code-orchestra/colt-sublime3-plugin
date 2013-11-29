e7771c3316b4f3eb7725545638946704354825d5
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-11-29 20:06:57 +0400

    Initial check-in

---
+++ colt_rpc.py
@@ -0,0 +1,236 @@
+import sublime
+import urllib
+import json
+import COLT.colt
+import calendar, time
+import os
+import threading
+
+from User.colt import ColtPreferences
+
+runAfterAuthorization = None
+statusToSet = "Disconnected from COLT"
+
+class ColtConnection(object):
+    port = -1
+    messageId = 1
+    activeSessions = 0
+
+def setStatus(status):
+    sublime.set_timeout(lambda: setStatus_(status), 0)    
+
+def setStatus_(status):
+    view = sublime.active_window().active_view()
+    if not view is None and not statusToSet is None :
+        view.erase_status("colt")
+        view.set_status("colt", status)
+
+def coltStateUpdate():    
+    if isConnected() :
+        ColtConnection.activeSessions = getActiveSessionsCount()
+        if ColtConnection.activeSessions > 0 :
+            # setStatus("COLT: " + str(ColtConnection.activeSessions) + " connections")    
+            setStatus("[~] Connected to COLT")    
+        else :
+            setStatus("Connected to COLT")    
+    else :
+        setStatus("Disconnected from COLT")
+            
+def isConnected():
+    return ColtConnection.port != -1
+
+def hasActiveSessions():
+    return ColtConnection.activeSessions > 0
+
+def disconnect():
+    ColtConnection.port = -1    
+    ColtConnection.messageId = 1
+    ColtConnection.activeSessions = 0
+
+    sublime.status_message("Disconnected from COLT")
+
+def runAfterAuthorization():
+    if not runAfterAuthorization is None :
+        runAfterAuthorization()
+        runAfterAuthorization = None
+        return
+
+def authorize(window):
+    if getSecurityToken() is None :
+        makeNewSecurityToken(True, window)
+    else :
+        runAfterAuthorization()
+
+def getSecurityToken(): 
+    settings = sublime.load_settings(ColtPreferences.NAME)
+    if not settings.has("securityToken") :
+            return None
+
+    return settings.get("securityToken")
+
+def makeNewSecurityToken(newRequest, window):
+    if newRequest :
+        try :
+            requestShortCode()
+        except Exception :
+            sublime.error_message("Can't request an authorization key from COLT. Make sure COLT is active and running")
+            return
+
+    window.show_input_panel("Enter the short key at the bottom of the screen displayed in COLT:", "", onShortKeyInput, None, None)
+
+def onShortKeyInput(shortCode):    
+    if shortCode :
+        try :
+            token = obtainAuthToken(shortCode)
+            if token is None :
+                sublime.error_message("Invalid short code entered")        
+                authorize()
+                return
+
+            settings = sublime.load_settings(ColtPreferences.NAME)
+            settings.set("securityToken", token)
+            sublime.save_settings(ColtPreferences.NAME)
+            sublime.status_message("Successfully authorized with COLT")
+
+            runAfterAuthorization()
+        except Exception:
+            sublime.error_message("Can't authorize with COLT. Make sure COLT is active and running")
+            return
+    else :
+        sublime.error_message("Short authorization key can't be empty")  
+        authorize(sublime.active_window())
+
+def obtainAuthToken(shortCode):
+    response = runRPC(ColtConnection.port, "obtainAuthToken", [ shortCode ])
+    if response.has_key("error") :
+            return None
+
+    return response["result"]
+
+def requestShortCode():
+    runRPC(ColtConnection.port, "requestShortCode", [ "Sublime Plugin" ])	    
+
+def runRPC(port, methodName, params):                  
+    jsonRequest = None
+    
+    messageId = ColtConnection.messageId
+    ColtConnection.messageId += 1
+
+    if (params is None) :
+            jsonRequest = { "jsonrpc" : "2.0", "method" : methodName, "id": messageId }
+    else :
+            jsonRequest = { "jsonrpc" : "2.0", "method" : methodName, "params": params, "id": messageId }                        
+
+    jsonRequestStr = json.dumps(jsonRequest)
+
+    url = "http://localhost:" + str(port) + "/rpc/coltService"
+    req = urllib.request.Request(url)
+    response = None
+
+    try :
+        response = urllib.request.urlopen(req, jsonRequestStr)
+    except Exception :
+        disconnect()
+        raise
+    
+    return json.loads(response.read())
+
+def reload():
+    return runRPC(ColtConnection.port, "reload", [ getSecurityToken() ])
+
+def startLive():
+    securityToken = getSecurityToken()
+    if not getSecurityToken() is None :                        
+        runRPC(ColtConnection.port, "startLive", [ securityToken ])
+
+def getState():
+    return runRPC(ColtConnection.port, "getState", None)
+
+def getActiveSessionsCount():
+    try :
+        jsonState = getState()
+        return len(jsonState["result"]["activeConnections"])
+    except Exception :
+        return 0
+
+def getDeclarationPosition(filePath, position, currentContent):
+    return runRPC(ColtConnection.port, "getDeclarationPosition", [ getSecurityToken(), filePath, position, currentContent ])
+
+def getContextForPosition(filePath, position, currentContent, contextType):
+    return runRPC(ColtConnection.port, "getContextForPosition", [ getSecurityToken(), filePath, position, currentContent, contextType ])
+
+def getMethodId(filePath, position, currentContent):
+    resultJSON = runRPC(ColtConnection.port, "getMethodId", [ getSecurityToken(), filePath, position, currentContent ])
+    if resultJSON.has_key("error") :
+        return None
+
+    return resultJSON["result"]
+
+def runMethod(methodId):
+    runRPC(ColtConnection.port, "runMethod", [ getSecurityToken(), methodId ])
+
+def establishConnection(port):
+    ColtConnection.port = port
+    sublime.status_message("Established connection with COLT on port " + port)
+    time.sleep(2)
+
+def initAndConnect(settings, projectPath): 
+    sublime.status_message("Trying to establish connection with COLT...")
+
+    port = locateCOLTServicePort(projectPath)
+    if not port is None :
+        establishConnection(port)
+        return port
+
+    COLT.colt.runCOLT(settings)
+    
+    timeout = 20
+    while timeout > 0 :
+        time.sleep(0.3)
+        timeout -= 0.3
+
+        port = locateCOLTServicePort(projectPath)
+        if not port is None :
+            establishConnection(port)
+            return port
+
+    sublime.error_message("Can't establish connection with COLT")
+    return None
+
+def locateCOLTServicePort(projectPath): 
+    port = getRPCPortForProject(projectPath)
+    if port is None :
+        return None
+
+    try :
+        runRPC(port, "ping", None)                        
+    except Exception:
+        return None
+
+    return port   
+
+def getRPCPortForProject(projectPath):
+    storageDir = COLT.colt.getProjectWorkingDir(projectPath)
+    if storageDir is None :
+        return None
+
+    rpcInfoFilePath = storageDir + os.sep + "rpc.info"
+    if not os.path.exists(rpcInfoFilePath) :
+        return None
+
+    timePassedSinceModification = int(calendar.timegm(time.gmtime())) - int(os.path.getmtime(rpcInfoFilePath))
+    if (timePassedSinceModification > 2) :
+        return None
+
+    with open(rpcInfoFilePath, "r") as rpcInfoFile :
+        return rpcInfoFile.read().split(":")[1]
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+set_interval(coltStateUpdate, 0.8)    
---
+++ run_with_colt.py
@@ -0,0 +1,277 @@
+import sublime, sublime_plugin
+import COLT.colt, COLT.colt_rpc
+import os.path
+import json
+
+from COLT.colt import ColtPreferences, isColtFile
+from COLT.colt_rpc import ColtConnection, isConnected, hasActiveSessions
+
+def getWordPosition(view):
+        position = getPosition(view)
+        return view.word(position).end()
+
+def getPosition(view):
+        for sel in view.sel() :
+                return sel
+
+        return None
+
+def getPositionEnd(view):
+        position = getPosition(view)
+        if position is None :
+                return - 1
+
+        return position.end()
+
+def getContent(view):
+        return view.substr(sublime.Region(0, view.size()))
+
+def isAutosaveEnabled():
+        settings = sublime.load_settings(ColtPreferences.NAME)
+        return settings.get("autosave", True)
+
+class ToggleAutosaveCommand(sublime_plugin.ApplicationCommand):
+        def run(self):
+                currentValue = isAutosaveEnabled()
+                settings = sublime.load_settings(ColtPreferences.NAME)
+                settings.set("autosave", not currentValue)
+                sublime.save_settings(ColtPreferences.NAME)
+
+        def description(self):
+                if isAutosaveEnabled() :
+                        return "Disable Autosave"
+                else :
+                        return "Enable Autosave"
+
+class ColtAutosaveListener(sublime_plugin.EventListener):
+        
+        def on_modified(self, view):
+                if isColtFile(view) and isConnected() and hasActiveSessions() and isAutosaveEnabled() :
+                        view.run_command("save")
+
+
+class ColtCompletitions(sublime_plugin.EventListener):
+        
+        def on_query_completions(self, view, prefix, locations):                
+                if not colt.isColtFile(view) :
+                        return []
+
+                position = getPositionEnd(view)
+
+                if view.substr(position - 1) == "." :
+                        position = position - 1
+                else :
+                        wordStart = view.word(getPosition(view)).begin()
+                        if view.substr(wordStart - 1) == "." :
+                                position = wordStart - 1
+                        else :
+                                return []
+
+                if not isConnected() or not hasActiveSessions() :
+                        return []
+
+                response = getContextForPosition(view.file_name(), position, getContent(view), "PROPERTIES")
+                if response.has_key("error") :
+                        return []
+
+                result = response["result"]
+                if not result is None :
+                        completitions = []
+                        resultJSON = json.loads(result)
+                        for resultStr in resultJSON :
+                                if "{})" in resultStr :
+                                        resultStr = resultStr.replace("{})", "")
+
+                                replaceStr = resultStr
+                                displayStr = resultStr
+                                cursiveStr = ""
+
+                                if "(" in resultStr :
+                                        replaceStr = resultStr[:resultStr.index("(")]
+                                        displayStr = replaceStr
+                                        cursiveStr = resultStr
+
+                                completitions.append((displayStr + "\t" + cursiveStr + "[COLT]", replaceStr))
+
+                return completitions
+
+class AbstractColtRunCommand(sublime_plugin.WindowCommand):
+        def run(self):
+                return
+
+        def getSettings(self):
+                settings = sublime.load_settings(ColtPreferences.NAME)
+                if not settings.has("coltPath") :                        
+                        sublime.error_message("COLT path is not specified, please enter the path")
+                        self.window.show_input_panel("COLT Path:", "", self.onCOLTPathInput, None, None)
+                        return
+
+                settings = sublime.load_settings(ColtPreferences.NAME)
+                coltPath = settings.get("coltPath")
+                
+                if not os.path.exists(coltPath) :
+                        sublime.error_message("COLT path specified is invalid, please enter the correct path")
+                        self.window.show_input_panel("COLT Path:", "", self.onCOLTPathInput, None, None)
+                        return
+
+                return sublime.load_settings(ColtPreferences.NAME)    
+
+        def onCOLTPathInput(self, inputPath):
+                if inputPath and os.path.exists(inputPath) :
+                        settings = sublime.load_settings(ColtPreferences.NAME)
+                        settings.set("coltPath", inputPath)
+                        sublime.save_settings(ColtPreferences.NAME)
+                        self.run()
+                else :
+                        sublime.error_message("COLT path specified is invalid")   
+
+        def is_enabled(self):
+                if self.window.active_view() is None :
+                        return False
+                return isColtFile(self.window.active_view())
+
+class ColtGoToDeclarationCommand(sublime_plugin.WindowCommand):
+
+        def run(self):
+                view = self.window.active_view()
+
+                fileName = view.file_name()
+                position = getWordPosition(view)
+                content = getContent(view)
+
+                resultJSON = COLT.colt_rpc.getDeclarationPosition(fileName, position, content)
+                if resultJSON.has_key("error") or resultJSON["result"] is None :
+                        # sublime.error_message("Can't find a declaration")
+                        return
+
+                position = resultJSON["result"]["position"]
+                filePath = resultJSON["result"]["filePath"]
+
+                targetView = self.window.open_file(filePath)
+                targetView.sel().clear()
+                targetView.sel().add(sublime.Region(position))
+                targetView.sel().add(sublime.Region(position))
+                
+                targetView.show_at_center(position)
+
+        def is_enabled(self):
+                view = self.window.active_view()
+                if view is None :
+                        return False
+                return isColtFile(view) and isConnected() and hasActiveSessions()
+
+class ColtRunFunctionCommand(sublime_plugin.WindowCommand):
+
+        def run(self):
+                view = self.window.active_view()
+
+                fileName = view.file_name()
+                position = getWordPosition(view)
+                content = getContent(view)
+                
+                methodId = COLT.colt_rpc.getMethodId(fileName, position, content)
+
+                if methodId is None :
+                        sublime.error_message("Can't figure out the function ID")
+                        return
+
+                if methodId.startswith('"') :
+                        methodId = methodId[1:len(methodId)-1]
+
+                COLT.colt_rpc.runMethod(methodId)
+        
+        def is_enabled(self):
+                view = self.window.active_view()
+                if view is None :
+                        return False
+                return isColtFile(view) and isConnected() and hasActiveSessions()
+
+class ColtViewValueCommand(sublime_plugin.WindowCommand):
+        def run(self):
+                view = self.window.active_view()
+
+                outputPanel = self.window.get_output_panel("COLT")
+                outputPanel.set_scratch(True)
+                outputPanel.set_read_only(False)
+                outputPanel.set_name("COLT")
+                self.window.run_command("show_panel", {"panel": "output.COLT"})
+                self.window.set_view_index(outputPanel, 1, 0)
+                
+                position = getWordPosition(view)
+                resultJSON = COLT.colt_rpc.getContextForPosition(view.file_name(), position, getContent(view), "VALUE")
+                if resultJSON.has_key("result") :
+                        position = getPosition(view)
+                        word = view.word(position)
+
+                        result = resultJSON["result"]
+                        if result is None :
+                                self.appendToConsole(outputPanel, view.substr(word) + " value: unknown")
+                        else :
+                                self.appendToConsole(outputPanel, view.substr(word) + " value: " + result)
+
+        def appendToConsole(self, outputPanel, text):
+                edit = outputPanel.begin_edit("COLT output")
+                edit = outputPanel.begin_edit()
+                outputPanel.insert(edit, outputPanel.size(), text + '\n')
+                outputPanel.end_edit(edit)
+                outputPanel.show(outputPanel.size())
+
+        def is_enabled(self):
+                view = self.window.active_view()
+                if view is None :
+                        return False
+                return isColtFile(view) and isConnected() and hasActiveSessions()
+        
+class ColtReloadCommand(sublime_plugin.WindowCommand):
+        def run(self):
+                COLT.colt_rpc.reload()
+
+        def is_enabled(self):
+                view = self.window.active_view()
+                if view is None :
+                        return False
+                return isColtFile(view) and isConnected() and hasActiveSessions()
+
+class StartColtCommand(AbstractColtRunCommand):
+
+        def run(self):
+                settings = self.getSettings()                
+                
+                # TODO: detect if colt is running and skip running it if it is
+                COLT.colt.runCOLT(settings)
+
+class OpenInColtCommand(AbstractColtRunCommand):
+
+        def run(self):
+                settings = self.getSettings()
+
+                # Export COLT project
+                coltProjectFilePath = COLT.colt.exportProject(self.window)
+
+                # Add project to workset file
+                COLT.colt.addToWorkingSet(coltProjectFilePath)
+
+                # Run COLT
+                COLT.colt_rpc.initAndConnect(settings, coltProjectFilePath)
+
+                # Authorize
+                COLT.colt_rpc.runAfterAuthorization = None
+                COLT.colt_rpc.authorize(self.window)                                                          
+
+class RunWithColtCommand(AbstractColtRunCommand):
+
+        def run(self):
+                settings = self.getSettings()
+
+                # Export COLT project
+                coltProjectFilePath = COLT.colt.exportProject(self.window)
+
+                # Add project to workset file
+                COLT.colt.addToWorkingSet(coltProjectFilePath)
+
+                # Run COLT
+                COLT.colt_rpc.initAndConnect(settings, coltProjectFilePath)
+
+                # Authorize and start live
+                COLT.colt_rpc.runAfterAuthorization = COLT.colt_rpc.startLive
+                COLT.colt_rpc.authorize(self.window)                                                          
